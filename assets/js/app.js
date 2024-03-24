1832f6d5e461e863f64017decd96a384ae183821
Eric-Coutinho <eric.coutinhop@hotmail.com>
2024-03-24 15:52:02 -0300

    Styles finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,59 @@
 function adicionaTarefaNaLista() {
-    const novaTarefa = document.getElementById('input_nova_tarefa').value
-    criaNovoItemDaLista(novaTarefa)
+    const novaTarefa = document.getElementById('input_nova_tarefa').value;
+    criaNovoItemDaLista(novaTarefa);
 }
 
 function criaNovoItemDaLista(textoDaTarefa) {
-    const listaTarefas = document.getElementById('lista_de_tarefas')
-    let qtdTarefas   = listaTarefas.children.length
+    const listaTarefas = document.getElementById('lista_de_tarefas');
+    let qtdTarefas = listaTarefas.children.length;
 
-    const novoItem = document.createElement('li')
+    const novoItem = document.createElement('li');
 
-    novoItem.innerText = textoDaTarefa
-    novoItem.id = `tarefa_id_${qtdTarefas++}`
+    novoItem.innerText = textoDaTarefa;
+    novoItem.id = `tarefa_id_${qtdTarefas++}`;
 
-    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id))
+    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id));
+    novoItem.appendChild(criaBotaoAtualizarTarefa(novoItem.id));
 
-    listaTarefas.appendChild(novoItem)
+    listaTarefas.appendChild(novoItem);
 }
 
-
 function criaInputCheckBoxTarefa(idTarefa) {
-    const inputTarefa = document.createElement('input')
-    inputTarefa.type = 'checkbox'
-    inputTarefa.setAttribute('onclick', `mudaEstadoTarefa('${idTarefa}')`)
-    return inputTarefa
+    const inputTarefa = document.createElement('input');
+    inputTarefa.type = 'checkbox';
+    inputTarefa.setAttribute('onclick', `mudaEstadoTarefa('${idTarefa}')`);
+    inputTarefa.setAttribute('class', 'checkbox');
+    return inputTarefa;
+}
+
+function criaBotaoAtualizarTarefa(idTarefa) {
+    const buttonUpdate = document.createElement('button');
+    buttonUpdate.setAttribute('onclick', `updateTarefa('${idTarefa}')`);
+    buttonUpdate.setAttribute('class', 'buttonUpdate');
+
+    const icon = document.createElement('img');
+    icon.setAttribute('src', 'assets/img/pen.png');
+    icon.setAttribute('class', 'icon');
+    
+    buttonUpdate.appendChild(icon);
+    return buttonUpdate;
 }
 
 function mudaEstadoTarefa(idTarefa) {
-    const tarefaSelecionada = document.getElementById(idTarefa)
+    const tarefaSelecionada = document.getElementById(idTarefa);
     if (tarefaSelecionada.style.textDecoration == 'line-through') {
-        tarefaSelecionada.style = 'text-decoration: none;'
+        tarefaSelecionada.style = 'text-decoration: none;';
     } else {
-        tarefaSelecionada.style = 'text-decoration: line-through;'
+        tarefaSelecionada.style = 'text-decoration: line-through;';
     }    
+}
+
+let isHidden = false;
+
+function escondeTarefa() {
+    isHidden = !isHidden;
+
+    if(isHidden = true) {
+        
+    }
 }
